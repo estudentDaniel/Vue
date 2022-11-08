844580efe8685c232e3d6bb375e36066ed4cc5f1
Daniel de castro <102565588+estudentDaniel@users.noreply.github.com>
2022-11-07 22:01:01 -0300

    inicio

---
+++ App Vue/appvue/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <primeiroappvue/>
+</template>
+
+<script>
+  import primeiroappvue from "./components/primeiroappvue.vue"
+
+  export default{
+    name: "App",
+    components: {
+      primeiroappvue
+    }
+  }
+</script>
---
+++ App Vue/appvue/src/components/primeiroappvue.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Deus é fiel!</h1>
+        <button v-on:click="apresentar">Apresentar</button>
+        <P> Meu nome é: {{nome}} e tenho :{{idade}} de idade </P>
+        <p>{{n}}</p>
+    </div>
+    
+</template>
+
+<script>
+export default{
+    name: 'primeiro-app-vue',
+    data(){
+        return{
+            nome: "Daniel de castro peres",
+            idade: 23,
+           
+        }
+    },
+    methods: {
+       
+        apresentar(){
+             const n = this.nome;
+        }
+    }
+}
+</script>

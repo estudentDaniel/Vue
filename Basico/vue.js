82bbafad103eedc9c179d23158ef9e2c7dc70050
Daniel de castro <102565588+estudentDaniel@users.noreply.github.com>
2022-11-15 11:07:55 -0300

    Update vue.js

---
+++ Basico/vue.js
@@ -0,0 +1,16 @@
+const apps = ({
+    data (){
+        return{
+            nome : "Daniel de castro"
+        }
+    },
+    methods: {
+       
+        alerta(){
+            
+            alert(this.nome)
+            console.log("Esta funcionando")
+        }
+    }
+});
+Vue.createApp(apps).mount("#app");

5a315204e5bc230957fe0fc99b97bcdc09fa0f39
Daniel de castro <102565588+estudentDaniel@users.noreply.github.com>
2022-11-29 21:07:40 -0300

    inicio

---
+++ anuncio/src/components/login.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>vendas plus</h1>
+        <div>
+           <input type="email" name="" id="">
+           <input type="password" name="" id="">
+
+            <button @click="entrar">Entrar</button>
+           <p>Esqueceu sua senha?</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'vendas-plus',
+    data(){
+        return{
+
+        }
+       
+    },
+    methods:{
+        entrar(){
+            console.log('entrou')
+        }
+    }
+     
+}
+</script>

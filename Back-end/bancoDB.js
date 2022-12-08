b56646320d19c0c9dff42dc547a2f70642ee575c
Daniel de castro <102565588+estudentDaniel@users.noreply.github.com>
2022-12-08 00:02:20 -0300

    falta pouco

--- Back-end/bancoDB.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect("mongodb://localhost:27017/admin").then(
-    ()=> {console.log("Banco conectado")}
-).catch(
-    ()=> {console.log("erro ao conectar")}
-)

f846264ce41b5a345eaa96b308de08c133655d62
Daniel de castro <102565588+estudentDaniel@users.noreply.github.com>
2022-12-13 22:12:50 -0300

    fim

---
+++ anuncio/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Navbar/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+   import '../src/components/Navbar.vue'
+export default{
+  components: {
+    Navbar: {
+
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
